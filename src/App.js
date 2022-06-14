d682707ffa20d24940eceb2ecc293c9f63e2ff7b
Starassassin-github <77239277+Starassassin-github@users.noreply.github.com>
2022-06-14 20:09:36 +0700

    ReactBasic-48Transtitions-52TranstitionsOtherLibaries
    ReactBasic-48Transtitions-52TranstitionsOtherLibaries

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import Users from './components/users'
-import { MyProvider } from './context'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+// COMPONENTS
+import Header from './components/header';
+import CssTr from './components/CSStransition';
+import Tgroup from './components/Tgroup';
+import TransitionComp from './components/Transition';
+import Other from './components/other'
+import Reducer from './components/reducer';
 
 const App = () => {
-  return (
-    <MyProvider>
-      <Users/>
-    </MyProvider>
+  return(
+    <BrowserRouter>
+      <Header/>
+      <div className="container">
+        <Routes>
+          <Route path="/transition" element={<TransitionComp/>}></Route>
+          <Route path="/csstransition" element={<CssTr/>}></Route>
+          <Route path="/tgroup" element={<Tgroup/>}></Route>
+          <Route path="/other" element={<Other/>}></Route>
+          <Route path="/reducer" element={<Reducer/>}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
-} 
+}
 
 export default App;
